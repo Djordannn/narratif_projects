da983e083843b2fdfc20e53526905dc98465186d
Djordannn <djordanfnby@gmail.com>
2024-11-05 11:00:25 +0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@
 
 import Cardshirt from "@/app/main_section/cardShirt";
 import Jumbotron from "@/app/main_section/jumbotron";
-import Sectionabout from "@/app/main_section/SectionAbout";
+import Sectionabout from "./main_section/sectionAbout";
 import Sectionprov from "@/app/main_section/sectionProvider";
-import Banner from "@/app/main_section/banner";
 import CardProduct from "@/app/main_section/cardProduct";
 import Gallery from "@/app/main_section/gallery";
 import Review from "@/app/main_section/review";
