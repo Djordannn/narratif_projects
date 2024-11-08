a775072f56b26fc9dbc54ea263b1176931107fed
Djordannn <djordanfnby@gmail.com>
2024-11-08 20:50:35 +0700

    Update link

--- src/app/link/page.tsx
+++ src/app/link/page.tsx
@@ -13,49 +13,49 @@ const Link = () => {
       </div>
       <div className="w-[80%] md:w-[60%] mx-auto">
         <ul className="flex flex-col gap-6 md:w-[50%] mx-auto">
-          <li className="flex items-center justify-between gap-4 bg-[#eeee] py-3 px-6 shadow-lg">
+          <li>
             <a
               href="https://shopee.co.id/c6u0mq247d?page=0&sortBy=pop&tab=0"
-              className="text-xl"
+              className="text-xl flex items-center justify-between gap-4 bg-[#eeee] py-3 px-6 shadow-lg"
             >
               official shoope
+              <p className="text-2xl bg-[#2d2d2d] p-3 text-white">
+                <TbBrandShopee />
+              </p>
             </a>
-            <p className="text-2xl bg-[#2d2d2d] p-3 text-white">
-              <TbBrandShopee />
-            </p>
           </li>
-          <li className="flex items-center justify-between gap-4 bg-[#eeee] py-3 px-6 shadow-lg">
+          <li>
             <a
               href="https://api.whatsapp.com/send?phone=6285712705266"
-              className="text-xl"
+              className="text-xl flex items-center justify-between gap-4 bg-[#eeee] py-3 px-6 shadow-lg"
             >
               admin
+              <p className="text-2xl bg-[#2d2d2d] p-3 text-white">
+                <FaWhatsapp />
+              </p>
             </a>
-            <p className="text-2xl bg-[#2d2d2d] p-3 text-white">
-              <FaWhatsapp />
-            </p>
           </li>
-          <li className="flex items-center justify-between gap-4 bg-[#eeee] py-3 px-6 shadow-lg">
+          <li className="">
             <a
               href="https://linktr.ee/clickhereforinfoadmin?fbclid=PAZXh0bgNhZW0CMTEAAabfkGXR4duIKrL63ayopI3EmgoYxBrysNy9vxh-hRnEu4UXt9nOWRDIY0I_aem_veq4_CVwHtr3loxLN7Uspg"
-              className="text-xl"
+              className="text-xl flex items-center justify-between gap-4 bg-[#eeee] py-3 px-6 shadow-lg"
             >
               linktree
+              <p className="text-2xl bg-[#2d2d2d] p-3 text-white">
+                <FaLink />
+              </p>
             </a>
-            <p className="text-2xl bg-[#2d2d2d] p-3 text-white">
-              <FaLink />
-            </p>
           </li>
-          <li className="flex items-center justify-between gap-4 bg-[#eeee] py-3 px-6 shadow-lg">
+          <li>
             <a
               href="https://www.tiktok.com/@narratif.apparel?is_from_webapp=1&sender_device=pc"
-              className="text-xl"
+              className="text-xl flex items-center justify-between gap-4 bg-[#eeee] py-3 px-6 shadow-lg"
             >
               tiktok
+              <p className="text-2xl bg-[#2d2d2d] p-3 text-white">
+                <FaTiktok />
+              </p>
             </a>
-            <p className="text-2xl bg-[#2d2d2d] p-3 text-white">
-              <FaTiktok />
-            </p>
           </li>
         </ul>
       </div>
